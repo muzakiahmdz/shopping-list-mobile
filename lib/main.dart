9be10b6892df6992d8e31d1e3cd71fabde4af314
muzakiahmdz <muzaki.ahmad@ui.ac.id>
2023-11-08 14:30:00 +0700

    tutorial 7 donn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/menu.dart';
+import 'package:shopping_list/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,13 +30,9 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.indigo,
-        ),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home:  MyHomePage(),
     );
   }
 }
-
